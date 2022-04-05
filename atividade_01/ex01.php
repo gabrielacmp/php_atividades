be316424ddeace837dd17ce4c8c16f9b13b3f379
Gabriela Rosa <gabrielarosacmp@gmail.com>
2022-04-05 11:09:43 -0300

    removendo as atividades comitadas no master

--- atividade_01/ex01.php
+++
@@ -1,8 +0,0 @@
-<?php
-    /* Rio, 13/03/22. Atividade 01 - PHP com Banco de Dados. Aluna: Gabriela Rosa de Vasconcellos */
-     echo "Exercício 01: Desenvolva um programa para dado o preço de um produto, calcular o novo preço com desconto de 9%. <br><br>";
-    $preco = 850;
-    $desconto = $preco - ($preco*0.9);
-    $preco_descontado = $preco - $desconto;
-    echo "Resposta: O desconto do produto, que custa {$preco} reais, foi de {$desconto} reais. Sendo o valor final {$preco_descontado} reais.";
-?>
--- atividade_01/ex03.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /* Rio, 13/03/22. Atividade 01 - PHP com Banco de Dados. Aluna: Gabriela Rosa de Vasconcellos*/
-    echo "Exercício 03: Escreva um programa que dados os litros gastos, os quilômetros percorridos por um automóvel e o valor do litro de combustível, calcule os gastos de combustível em reais/km.<br><br>";
-    $quilometros = 1200;
-    $litro_gasto = 15;
-    $valor_litro = 5;
-    $gasto_combustivel = (($valor_litro*$litro_gasto)/$quilometros); 
-    echo "Resposta: Dado a quilometragem do percurso ({$quilometros} km), o total de litros gastos ({$litro_gasto} litros) e o valor do litro do combustível ({$valor_litro} reais), o cálculo do gasto do automóvel foi de {$gasto_combustivel} reais/km.";
-?>
